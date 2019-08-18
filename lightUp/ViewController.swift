fc82b7ca1c836a8f89294333a2d2c6cb22767f04
John Towery <kbaries@gmail.com>
2019-08-18 14:22:57 -0600

    Initial Commit

---
+++ lightUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lightUp
+//
+//  Created by John Towery on 8/18/19.
+//  Copyright © 2019 John Towery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
