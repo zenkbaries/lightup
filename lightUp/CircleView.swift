24c20e7ed768c955060f76900bd379e333190d71
John Towery <kbaries@gmail.com>
2019-08-26 16:46:01 -0600

    Push all change so far

---
+++ lightUp/CircleView.swift
@@ -0,0 +1,51 @@
+//
+//  CircleView.swift
+//  lightUp
+//
+//  Created by John Towery on 8/18/19.
+//  Copyright © 2019 John Towery. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // Get the Graphics Context
+        if let context = UIGraphicsGetCurrentContext() {
+            
+            // Set the circle outerline-width
+            context.setLineWidth(5.0);
+            
+            // Set the circle outerline-colour
+            UIColor.red.set()
+            
+            // Create Circle
+            let center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+            let radius = (frame.size.width - 10)/2
+            context.addArc(center: center, radius: radius, startAngle: 0.0, endAngle: .pi * 2.0, clockwise: true)
+            
+            // Draw
+            context.strokePath()
+        }
+    }
+    
+    
+}
--- lightUp/ViewController.swift
+++ lightUp/ViewController.swift
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.black
         // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            // Set the Center of the Circle
+            // 1
+            let circleCenter = touch.location(in: view)
+            
+            // Set a random Circle Radius
+            // 2
+            let circleWidth = CGFloat(25 + (arc4random() % 50))
+            let circleHeight = circleWidth
+            
+            // Create a new CircleView
+            // 3
+            let circleView = CircleView(frame: CGRect(x: circleCenter.x, y: circleCenter.y, width: circleWidth, height: circleHeight))
+            view.addSubview(circleView)
+        }
     }
 
 
